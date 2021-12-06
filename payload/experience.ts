bb9cd02258d08af66ecc8e6ed4a7a09f037dd8f7
정종범 <jay@weltcorp.com>
2021-12-06 22:15:03 +0900

    Update experiences

--- payload/experience.ts
+++ payload/experience.ts
@@ -4,13 +4,26 @@ const experience: IExperience.Payload = {
   disable: false,
   list: [
     {
+      title: '현대카드',
+      position: 'ML Engineer(Associate)',
+      startedAt: '2021-12',
+      descriptions: [
+        'MLOps',
+        'Data pipeline 구축',
+        '현대카드 3층 서비스(개인 맞춤형 할인, 쿠폰, 이벤트 추천) 운영',
+      ],
+      skillKeywords: ['Node.js', 'Kubernetes', 'Airflow', 'ArgoCD', 'Kubeflow'],
+    },
+    {
       title: 'WELT(주)',
       position: '인턴 / ICT 학점연계 프로젝트 인턴십',
       startedAt: '2021-03',
+      endedAt: '2021-12',
       descriptions: [
         '백엔드 개발 업무 담당',
         '실시간 로그, 에러 알림 시스템 개발 담당',
         'Audit Log(감사 로그) 시스템 개발',
+        '참여수기 공모전 우수상 수상',
       ],
       skillKeywords: ['Node.js', 'Kubernetes', 'Redis', 'RabbitMQ', 'PostgreSQL'],
     },
